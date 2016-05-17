61df195041a3246ad5b5efbd865335dea0716355
Jeremy Landi <Jeremy Landi>
2016-05-17 11:52:41 -0700

    addingFileUpload

--- src/MvcZoolandia/Models/Animal.cs
+++ src/MvcZoolandia/Models/Animal.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,8 +9,12 @@ namespace MvcZoolandia.Models
     public class Animal
     {
         public int ID { get; set; }
+
+        [RegularExpression(@"^[A-Z]+[a-zA-Z''-'\s]*$"), Required, StringLength(30)]
         public string Name { get; set; }
+        [Required]
         public int IdHabitat { get; set; }
+        [Required]
         public int IdSpecies { get; set; }
     }
 }
